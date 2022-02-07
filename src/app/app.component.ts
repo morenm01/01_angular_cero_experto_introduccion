90bb0198e190b832d7004358baab1de086156389
miriam <m.moreno.clavijo@gmail.com>
2022-02-07 17:20:25 +0100

    01-using template

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  //templateUrl: './app.component.html',//si ponemos template
+  template: `
+  <h1>Hola de nuevo</h1>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'bases';
+}
