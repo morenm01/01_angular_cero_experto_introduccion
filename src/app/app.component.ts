1b2f000cf1e302d56b26952e0efbc5211f27766f
miriam <m.moreno.clavijo@gmail.com>
2022-02-07 18:04:48 +0100

    using click event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-titulo = 'Contador';
+titulo: string = 'Contador';
+numero: number = 10;
 }
