84a2fc49d016273bb9dccfd81f63bbe47e3aeb26
miriam <m.moreno.clavijo@gmail.com>
2022-02-09 16:23:08 +0100

    comunicacion padre-hijo @Input

---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html',
+  styleUrls: ['./personajes.component.css']
+})
+export class PersonajesComponent  {
+
+  @Input('data') personajes: any []=[];
+
+}
