b895c7803f9bdbf3d596eae7445ecda04e8fae85
miriam <m.moreno.clavijo@gmail.com>
2022-02-09 14:02:01 +0100

    mostrando el listado de personajes

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -12,14 +12,34 @@ interface Personaje{
 export class MainPageComponent  {
 
   nuevo: Personaje = {
-    nombre: "Trucks",
-    poder: 15000
+    nombre: "",
+    poder:0
   }
+  personajes: Personaje[] = [
+    {
+      nombre: "Goku",
+      poder: 15000
+    },
+    {
+      nombre: "Krillin",
+      poder: 900
+    },
+    {
+      nombre: "Vegeta",
+      poder: 700
+    },
+  ]
 
   agregar(){
+
+    if(this.nuevo.nombre.trim().length===0){ return;}
+
     console.log(`${this.nuevo.nombre} - ${this.nuevo.poder}`);
-
-
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre:"",
+      poder : 0
+    }
   }
   cambiarNombre(event:any){
     console.log(event.target.value);
